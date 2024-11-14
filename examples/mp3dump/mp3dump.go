cd89ce7ac889b6802793783673e976a4f1356179
david.dai <david.dai@xbotspace.com>
2024-11-15 02:24:05 +0800

    change go version

--- examples/mp3dump/mp3dump.go
+++ examples/mp3dump/mp3dump.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/bobertlo/go-mpg123/mpg123"
+	"github.com/davidapp/go-mpg123/mpg123"
 	"os"
 )
 
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/bobertlo/go-mpg123
+module github.com/davidapp/go-mpg123
 
-go 1.17
+go 1.22
